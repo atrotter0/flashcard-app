3c0f5316de511cb84a0064bd3dbd0969f9da8f97
Student <example@example.com>
2018-08-27 10:37:29 -0700

    create log-in component, html for form.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,12 +1,17 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { LogInComponent } from '../app/log-in/log-in.component';
 
 const appRoutes: Routes = [
   {
     path: '',
     component: AppComponent
   },
+  {
+  path: 'log-in',
+  component: LogInComponent
+  }
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
