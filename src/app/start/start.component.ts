46006d0a7ba7e82c57fbba13bb40e648e3e69609
Kevin Ahn <kevinahn7@hotmail.com>
2018-08-27 11:51:36 -0700

    Set up start component to get a deck service

--- src/app/start/start.component.ts
+++ src/app/start/start.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Deck } from '../models/deck.model';
 import { Decks } from '../models/decks.model';
 import { Question } from '../models/question.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-start',
@@ -12,9 +13,10 @@ export class StartComponent implements OnInit {
   currentDeck: Deck;
   currentQuestion: Question;
 
-  constructor() { }
+  constructor(private router: Router, private deckService: DeckService) { }
 
   ngOnInit() {
+    this.currentDeck = this.deckService.getDeck();
   }
 
 }
