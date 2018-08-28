08a9dcf116f4063103a9aaf4354bcab5b2293316
Kevin Ahn <kevinahn7@hotmail.com>
2018-08-28 09:39:20 -0700

    Add questions model

--- src/app/services/question.service.ts
+++ src/app/services/question.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Deck } from '../models/deck.model';
+import { Decks } from '../models/decks.model';
+import { Question } from '../models/question.model';
+import { Questions } from '../models/questions.model';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Injectable()
 export class QuestionService {
+  questions: FirebaseListObservable<any[]>;
+  constructor() {
+    this.questions = database.list('questions');
+  }
 
-  constructor() { }
+  getQuestionsByCategory(category: string) {
+    let categoryQuestions: Question[] = [];
+
+  }
 
 }
