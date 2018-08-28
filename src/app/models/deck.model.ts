a7a915c5a210980c392aefb4aa8ff52dd73190d0
david <luandavidn@gmail.com>
2018-08-28 15:08:05 -0700

    begin adding tag property to deck model

--- src/app/models/deck.model.ts
+++ src/app/models/deck.model.ts
@@ -4,6 +4,7 @@ export class Deck {
   //add this back to constructor when user is wired up
   public userId: string;
   public questions: Question[];
+  public tag: string[];
 
   constructor(
     public name: string,
