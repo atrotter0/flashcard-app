2775bc68a6c800be0ce652d645597e35e16bc18c
Kevin Ahn <kevinahn7@hotmail.com>
2018-08-29 13:51:25 -0700

    Refactor more the runAddAllQuestionsToDeck method

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -47,12 +47,10 @@ export class CategoryComponent implements OnInit {
   }
 
   runAddQuestionToDeck(question: Question) {
-    for (let i = 0; i < this.chosenDeck.questions.length; i++) {
-      if (Object.getOwnPropertyNames(this.chosenDeck.questions[i])[0].toLowerCase() === question.category.toLowerCase()) {
-        this.chosenDeck.questions[question.category.toLowerCase()].push(question);
-      } else {
-        this.chosenDeck.questions[question.category.toLowerCase()] = [question];
-      }
+    if (question.category.toLowerCase() in this.chosenDeck.questions) {
+      this.chosenDeck.questions[question.category.toLowerCase()].push(question);
+    } else {
+      this.chosenDeck.questions[question.category.toLowerCase()] = [question];
     }
     this.deckService.updateQuestionsInDeck(this.chosenDeck);
   }
@@ -69,12 +67,10 @@ export class CategoryComponent implements OnInit {
   }
 
   runAddAllQuestionsToDeck() {
-    for (let i = 0; i < this.chosenDeck.questions.length; i++) {
-      if (Object.getOwnPropertyNames(this.chosenDeck.questions[i])[0].toLowerCase() === this.categoryQuestions[0].category.toLowerCase()) {
-        this.chosenDeck.questions[this.categoryQuestions[0].category.toLowerCase()].push(this.categoryQuestions);
-      } else {
-        this.chosenDeck.questions[this.categoryQuestions[0].category.toLowerCase()] = [this.categoryQuestions];
-      }
+    if (question.category.toLowerCase() in this.chosenDeck.questions) {
+      this.chosenDeck.questions[this.categoryQuestions[0].category.toLowerCase()].push(this.categoryQuestions);
+    } else {
+      this.chosenDeck.questions[this.categoryQuestions[0].category.toLowerCase()] = [this.categoryQuestions];
     }
     this.deckService.updateQuestionsInDeck(this.chosenDeck);
   }
