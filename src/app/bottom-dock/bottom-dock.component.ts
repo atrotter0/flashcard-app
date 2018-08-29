b1427b6a44a00fb5f8de6e10731802b5c1888844
dn <luandavidn@gmail.com>
2018-08-29 12:54:39 -0700

    make a general commit after forgetting what's happened since lunch

--- src/app/bottom-dock/bottom-dock.component.ts
+++ src/app/bottom-dock/bottom-dock.component.ts
@@ -2,21 +2,29 @@ import { Component, OnInit } from '@angular/core';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 import { DeckService } from '../services/deck.service';
 import { Deck } from '../models/deck.model';
+import { Question } from '../models/question.model';
+import { AuthenticationService } from '../services/authentication.service';
+import * as firebase from "firebase";
 
 @Component({
   selector: 'app-bottom-dock',
   templateUrl: './bottom-dock.component.html',
   styleUrls: ['./bottom-dock.component.css'],
-  providers: [DeckService]
+  providers: [DeckService, AuthenticationService]
 })
 export class BottomDockComponent implements OnInit {
+  private user;
   creatingDeck: boolean = false;
 
-  constructor(private service: DeckService) {
+  constructor(private deckService: DeckService, public authService: AuthenticationService) {
 
   }
 
   ngOnInit() {
+  }
+
+  ngDoCheck() {
+    this.user = firebase.auth().currentUser;
   }
 
   select(value) {
@@ -24,8 +32,17 @@ export class BottomDockComponent implements OnInit {
     else this.creatingDeck = false;
   }
 
-  createDeck(name) {
-    this.service.createDeck(name);
+  runCreateDeck(deckName) {
+    let newDeck = new Deck(deckName);
+    this.deckService.createDeck(newDeck);
+    console.log(this.user);
+    console.log(this.user.decks);
+    this.user.decks.push(newDeck);
+    this.deckService.addDeckToUser(this.user);
+  }
+
+  tossDeck(deckId) {
+    console.log(deckId);
   }
 
 }
