241f77d62e47eaa0029cbc5de90d8fb19933f62c
Abel Trotter <a_trotter0@hotmail.com>
2018-08-30 00:49:17 -0700

    Add categoryNames to drop-down in question-add component

--- src/app/question-add/question-add.component.ts
+++ src/app/question-add/question-add.component.ts
@@ -4,22 +4,30 @@ import { AuthenticationService } from '../services/authentication.service';
 import { Question } from '../models/question.model';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import * as firebase from "firebase";
+import { CategoryService } from '../services/category.service';
+import { FirebaseListObservable } from '../../../node_modules/angularfire2/database';
 
 @Component({
   selector: 'app-question-add',
   templateUrl: './question-add.component.html',
   styleUrls: ['./question-add.component.css'],
-  providers: [QuestionService, AuthenticationService]
+  providers: [QuestionService, AuthenticationService, CategoryService]
 })
 export class QuestionAddComponent implements OnInit {
-  userQuestions: Question[];
   private user;
+  userQuestions: Question[];
+  categories: FirebaseListObservable<any[]>;
+  categoryNames: String[] = [];
 
   constructor(
     public qService: QuestionService,
     public authService: AuthenticationService,
-    private router: Router
-  ) { }
+    private router: Router,
+    public categoryService: CategoryService
+  ) {
+      this.categories = this.categoryService.getCategories();
+      this.getCategoryNames();
+    }
 
   ngDoCheck(){
     this.user = firebase.auth().currentUser;
@@ -35,5 +43,18 @@ export class QuestionAddComponent implements OnInit {
 
   goToQuestionDetail() {
     this.router.navigate(['questions']);
+  }
+
+  getCategoryNames() {
+    this.categories.subscribe((data) => {
+      let categoryObjects = data;
+      this.setCategoryNames(categoryObjects);
+    });
+  }
+
+  setCategoryNames(categoryObjects: any[]) {
+    categoryObjects.forEach((category) => {
+      this.categoryNames.push(category.name);
+    });
   }
 }
